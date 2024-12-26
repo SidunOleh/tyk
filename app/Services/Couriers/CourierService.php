ed752498b34d11cf975268d9e42d9f450ecb3648
Oleh Sidun <sidunoleh@gmail.com>
2024-12-26 20:24:03 +0200

    fix

--- app/Services/Couriers/CourierService.php
+++ app/Services/Couriers/CourierService.php
@@ -7,11 +7,30 @@ use App\Http\Requests\Admin\Couriers\Cashes\UpdateRequest as CashesUpdateRequest
 use App\Models\Cash;
 use App\Models\Courier;
 use App\Services\Service;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 
 class CourierService extends Service
 {
     protected string $model = Courier::class;
+
+    public function index(Request $request): LengthAwarePaginator
+    {
+        $page = $request->query('page', 1);
+        $perpage = $request->query('perpage', 15);
+        $orderby = $request->query('orderby', 'created_at');
+        $order = $request->query('order', 'DESC');
+        $s = $request->query('s', '');
+        $vehicles = $request->query('vehicles', []);
+
+        $models = $this->model::orderBy($orderby, $order)
+            ->search($s)
+            ->vehicles($vehicles)
+            ->paginate($perpage, ['*'], 'page', $page);
+
+        return $models;
+    }
 
     public function all(): Collection
     {
