12176ba850a1c302e0f3c13fd8533f63ab411394
Oleh Sidun <sidunoleh@gmail.com>
2025-03-20 00:24:47 +0200

    fix

--- resources/views/pages/order-car.blade.php
+++ resources/views/pages/order-car.blade.php
@@ -298,7 +298,7 @@
 <script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css"/>
 <script type="module">
-import { createApp, reactive } from '{{ asset("/assets/js/petite-vue.es.js") }}'
+import { createApp, reactive, } from '{{ asset("/assets/js/petite-vue.es.js") }}'
 
 const getAddressFromPlace = place => {
     let address = place.formatted_address
