90d2e8e4359844b4aea793093c74e26d5fd5fa15
Oleh Sidun <sidunoleh@gmail.com>
2025-05-03 11:25:34 +0300

    fix

--- resources/js/api/courier-services.js
+++ resources/js/api/courier-services.js
@@ -2,7 +2,7 @@ import { hasRole } from '../helpers/helpers'
 
 export default {
     async all() {
-        if (!hasRole(['адмін', ])) {
+        if (!hasRole(['адмін', 'диспетчер', ])) {
             throw new Error('Заборонено.')
         }
 
